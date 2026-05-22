919f95db8abd7e8097d85f487829b986194af023
Potato <danielfcg4@gmail.com>
2026-05-21 20:40:52 -0500

    updated

--- discord_bot.py
+++ discord_bot.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('TOKEN')
+TOKEN = os.getenv('DISCORD')
 
 intents = discord.Intents.default()
 intents.message_content = True
